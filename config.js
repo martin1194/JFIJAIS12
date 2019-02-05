db253156cf0bf697647744e2413a50181021f391
martin1194 <martinc1194@gmail.com>
2019-02-06 05:04:28 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-const stat = 'Online'
-
- { "stat": "Online",
-  "activity": "測試中",
-  "time": "15:08",
-  "command": "Online/idle/invisible/dnd"
-  }
+const stat = 'Online';
+const activity = "測試中";
+const time = "00:00";
+const command = "Online/idle/invisible/dnd";
